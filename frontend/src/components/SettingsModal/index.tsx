7c178fe28a385937dad911369ef210340e452e1f
pedrolabrador <pedrolabrador3@outlook.com>
2024-04-02 14:06:21 -0400

    SFC-39 redesign interface

--- frontend/src/components/SettingsModal/index.tsx
+++ frontend/src/components/SettingsModal/index.tsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from "react";
-import { SettingsFilled, SaveFilled } from "@fluentui/react-icons";
+import React, { useState, useEffect, useCallback } from "react";
+import { SettingsFilled, SaveFilled, ErrorCircleFilled } from "@fluentui/react-icons";
+import { Checkbox } from "@fluentui/react/lib/Checkbox";
+import { TooltipHost, TooltipDelay, DirectionalHint, DefaultButton, Modal, Stack, Text, Spinner, Slider } from "@fluentui/react";
 import styles from "./SettingsModal.module.css";
 import { getSettings, postSettings } from "../../api/api";
-
-import { DefaultButton, Modal, Stack, Text, TextField, Spinner } from "@fluentui/react";
+import { mergeStyles } from "@fluentui/react/lib/Styling";
 
 interface Props {
     user: {
@@ -12,21 +13,46 @@ interface Props {
     } | null;
 }
 
+const iconClass = mergeStyles({
+    fontSize: 25,
+    height: 25,
+    width: 25,
+    margin: "0",
+    padding: "5px 0 0 0px",
+    cursor: "pointer"
+});
+
+const itemClass = mergeStyles({
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: "10px 0"
+});
+
 const SettingsModal = ({ user }: Props) => {
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const [temperature, setTemperature] = useState("");
-    const [presencePenalty, setPresencePenalty] = useState("");
-    const [frequencyPenalty, setFrequencyPenalty] = useState("");
+    const [temperature, setTemperature] = useState("0");
+    const [presencePenalty, setPresencePenalty] = useState("0");
+    const [frequencyPenalty, setFrequencyPenalty] = useState("0");
     const [loading, setLoading] = useState(true);
+
+    const temperatureDialog =
+        "It adjusts the balance between creativity and predictability in responses. Lower settings yield straightforward answers, while higher settings introduce originality and diversity, perfect for creative tasks and factual inquiries.";
+    const frequencyPenaltyDialog =
+        "Streamlines dialogue by minimizing repetition. Increase to boost variety and prevent redundancy; decrease to ensure key terms recur, enhancing focus on specific concepts. Use it to decrease excessive repetition.";
+    const presencePenaltyDialog =
+        "Promotes the introduction of new topics and ideas. Increase to discover varied concepts; decrease to maintain focus on current discussions. Ideal for brainstorming and exploration.";
 
     useEffect(() => {
         const fetchData = async () => {
-            getSettings({ user }).then(data => {
-                setTemperature(data.temperature);
-                setPresencePenalty(data.presencePenalty);
-                setFrequencyPenalty(data.frequencyPenalty);
-                setLoading(false);
-            });
+            getSettings({ user })
+                .then(data => {
+                    setTemperature(data.temperature);
+                    setPresencePenalty(data.presencePenalty);
+                    setFrequencyPenalty(data.frequencyPenalty);
+                    setLoading(false);
+                })
+                .catch(error => setLoading(false));
         };
 
         setLoading(true);
@@ -42,7 +68,8 @@ const SettingsModal = ({ user }: Props) => {
     };
 
     const handleSubmit = () => {
-        if (!temperature || !presencePenalty || !frequencyPenalty) {
+        if ((!temperature && temperature != "0") || (!presencePenalty && presencePenalty != "0") || (!frequencyPenalty && frequencyPenalty != "0")) {
+            console.error("Invalid settings are not submitted.");
             return;
         }
 
@@ -74,22 +101,39 @@ const SettingsModal = ({ user }: Props) => {
     };
 
     const handleSetTemperature = (val: any) => {
-        if (validateValue(val.target.value, setTemperature)) {
-            setTemperature(val.target.value);
+        const value = String(val);
+        if (validateValue(value, setTemperature)) {
+            setTemperature(value);
         }
     };
 
-    const handleSetPresencePenalty = (val: any) => {
-        if (validateValue(val.target.value, setPresencePenalty)) {
-            setPresencePenalty(val.target.value);
-        }
-    };
+    const handleSetPresencePenalty = useCallback((ev?: React.FormEvent<HTMLElement | HTMLInputElement>, checked?: boolean): void => {
+        const presencePenalty = !!checked ? "1" : "0";
+        setPresencePenalty(presencePenalty);
+    }, []);
 
-    const handleSetFrequencyPenalty = (val: any) => {
-        if (validateValue(val.target.value, setFrequencyPenalty)) {
-            setFrequencyPenalty(val.target.value);
-        }
-    };
+    const handleSetFrequencyPenalty = useCallback((ev?: React.FormEvent<HTMLElement | HTMLInputElement>, checked?: boolean): void => {
+        const frequencyPenalty = !!checked ? "1" : "0";
+        setFrequencyPenalty(frequencyPenalty);
+    }, []);
+
+    const onRenderLabel = (dialog: string, title: string) => (
+        <TooltipHost
+            tooltipProps={{
+                onRenderContent: () => (
+                    <div>
+                        <h3>{title}</h3>
+                        {dialog}
+                    </div>
+                )
+            }}
+            delay={TooltipDelay.zero}
+            directionalHint={DirectionalHint.bottomCenter}
+            styles={{ root: { display: "inline-block" } }}
+        >
+            <ErrorCircleFilled className={iconClass} />
+        </TooltipHost>
+    );
 
     return (
         <div>
@@ -100,7 +144,7 @@ const SettingsModal = ({ user }: Props) => {
 
             <Modal isOpen={isModalOpen} onDismiss={hideModal} isBlocking>
                 <Stack className={`${styles.answerContainer}`} verticalAlign="space-between">
-                    <Stack.Item grow>
+                    <Stack.Item grow className={styles["w-100"]}>
                         <div className="header">
                             <h2>Adjust your settings</h2>
                             <DefaultButton onClick={hideModal} className={styles.closeButton}>
@@ -119,10 +163,42 @@ const SettingsModal = ({ user }: Props) => {
                                 <h3 style={{ textAlign: "center" }}>Loading your settings</h3>
                             </div>
                         ) : (
-                            <div>
-                                <TextField label="Temperature" value={temperature} onChange={e => handleSetTemperature(e)} />
-                                <TextField label="Frequency Penalty" value={frequencyPenalty} onChange={e => handleSetFrequencyPenalty(e)} />
-                                <TextField label="Presence Penalty" value={presencePenalty} onChange={e => handleSetPresencePenalty(e)} />
+                            <div className={styles["w-100"]}>
+                                <div className={styles["w-100"]}>
+                                    <div className={itemClass}>
+                                        <span>Creativity Scale</span>
+                                        {onRenderLabel(temperatureDialog, "Temperature")}
+                                    </div>
+                                    <Slider
+                                        className={styles["w-100"]}
+                                        label=""
+                                        min={0}
+                                        max={1}
+                                        step={0.1}
+                                        defaultValue={0}
+                                        showValue
+                                        snapToStep
+                                        onChange={e => handleSetTemperature(e)}
+                                    />
+                                </div>
+                                <div className={itemClass}>
+                                    <span>Variety Boost</span>
+                                    <Checkbox
+                                        label=""
+                                        checked={frequencyPenalty === "1"}
+                                        onChange={handleSetFrequencyPenalty}
+                                        onRenderLabel={() => onRenderLabel(frequencyPenaltyDialog, "Frequency Penalty")}
+                                    />
+                                </div>
+                                <div className={itemClass}>
+                                    <span>Topic Explorer</span>
+                                    <Checkbox
+                                        label=""
+                                        checked={presencePenalty === "1"}
+                                        onChange={handleSetPresencePenalty}
+                                        onRenderLabel={() => onRenderLabel(presencePenaltyDialog, "Presence Penalty")}
+                                    />
+                                </div>
                                 <DefaultButton className={styles.saveButton} onClick={handleSubmit}>
                                     <SaveFilled />
                                     &#8202;&#8202;Save
