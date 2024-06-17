45ae7ed6f2155bf2f2893ececfccc9da5baf57e6
Alejandro Lopez <alexjes6543@gmail.com>
2024-06-17 13:19:26 -0400

    Modify userList

--- frontend/src/pages/admin/Admin.tsx
+++ frontend/src/pages/admin/Admin.tsx
@@ -1,145 +1,165 @@
-import React from "react";
-import { Separator, PrimaryButton } from "@fluentui/react";
-import { Announced } from '@fluentui/react/lib/Announced';
-import { TextField, ITextFieldStyles } from '@fluentui/react/lib/TextField';
-import { DetailsList, DetailsListLayoutMode, Selection, IColumn } from '@fluentui/react/lib/DetailsList';
-import { MarqueeSelection } from '@fluentui/react/lib/MarqueeSelection';
-import { mergeStyles } from '@fluentui/react/lib/Styling';
-import { Text } from '@fluentui/react/lib/Text';
-
-import { Link } from '@fluentui/react/lib/Link';
-import { Image, ImageFit } from '@fluentui/react/lib/Image';
-import { buildColumns } from '@fluentui/react/lib/DetailsList';
-
-
+import React, { ReactNode } from "react";
+import { PrimaryButton, IconButton } from "@fluentui/react";
+import { Announced } from "@fluentui/react/lib/Announced";
+import { TextField, ITextFieldStyles } from "@fluentui/react/lib/TextField";
+import { DetailsList, DetailsListLayoutMode, Selection, IColumn } from "@fluentui/react/lib/DetailsList";
+import { MarqueeSelection } from "@fluentui/react/lib/MarqueeSelection";
+import { mergeStyles } from "@fluentui/react/lib/Styling";
 
 import styles from "./Admin.module.css";
 
 const exampleChildClass = mergeStyles({
-  display: 'block',
-  marginBottom: '10px',
+    display: "block",
+    marginBottom: "10px"
 });
 
-const textFieldStyles: Partial<ITextFieldStyles> = { root: { maxWidth: '300px' } };
+const textFieldStyles: Partial<ITextFieldStyles> = { root: { maxWidth: "300px" } };
 
-export interface IDetailsListBasicExampleItem {
-  key: number;
-  name: string;
-  value: number;
+export interface IUserListItem {
+    key: number;
+    name: string;
+    email: string;
+    role: string;
+    actions: ReactNode;
+    value: number;
 }
 
-export interface IDetailsListBasicExampleState {
-  items: IDetailsListBasicExampleItem[];
-  selectionDetails: string;
+export interface IUserListState {
+    items: IUserListItem[];
+    selectionDetails: string;
 }
 
-export class DetailsListBasicExample extends React.Component<{}, IDetailsListBasicExampleState> {
-  private _selection: Selection;
-  private _allItems: IDetailsListBasicExampleItem[];
-  private _columns: IColumn[];
+export class UserList extends React.Component<{}, IUserListState> {
+    private _selection: Selection;
+    private _allItems: IUserListItem[];
+    private _columns: IColumn[];
+    
 
-  constructor(props: {}) {
-    super(props);
+    constructor(props: {}) {
+        super(props);
 
-    this._selection = new Selection({
-      onSelectionChanged: () => this.setState({ selectionDetails: this._getSelectionDetails() }),
-    });
+        this._selection = new Selection({
+            onSelectionChanged: () => this.setState({ selectionDetails: this._getSelectionDetails() })
+        });
 
-    // Populate with items for demos.
-    this._allItems = [];
-    for (let i = 0; i < 200; i++) {
-      this._allItems.push({
-        key: i,
-        name: 'Item ' + i,
-        value: i,
-      });
+        // Populate with items for demos.
+        this._allItems = [];
+        for (let i = 0; i < 10; i++) {
+            this._allItems.push({
+                key: i,
+                name: "User " + i,
+                email: "example@mail.com",
+                role: "User",
+                actions: actions({}),
+                value: i
+            });
+        }
+
+        this._columns = [
+            { key: "column1", name: "Name", fieldName: "name", minWidth: 100, maxWidth: 200, isResizable: true },
+            { key: "column2", name: "Email", fieldName: "email", minWidth: 100, maxWidth: 200, isResizable: true },
+            { key: "column3", name: "Role", fieldName: "role", minWidth: 100, maxWidth: 200, isResizable: true },
+            { key: "column4", name: "Actions", fieldName: "actions", minWidth: 300, maxWidth: 400, isResizable: true }
+        ];
+
+        this.state = {
+            items: this._allItems,
+            selectionDetails: this._getSelectionDetails()
+        };
     }
 
-    this._columns = [
-      { key: 'column1', name: 'Name', fieldName: 'name', minWidth: 100, maxWidth: 200, isResizable: true },
-      { key: 'column2', name: 'Email', fieldName: 'value', minWidth: 100, maxWidth: 200, isResizable: true },
-      { key: 'column3', name: 'Actions', fieldName: 'value', minWidth: 300, maxWidth: 400, isResizable: true },
-      { key: 'column4', name: 'Role', fieldName: 'value', minWidth: 100, maxWidth: 200, isResizable: true },
-    ];
+    public render(): JSX.Element {
+        const { items, selectionDetails } = this.state;
 
-    this.state = {
-      items: this._allItems,
-      selectionDetails: this._getSelectionDetails(),
+        return (
+            <div>
+                <div className={exampleChildClass}>{selectionDetails}</div>
+                <Announced message={selectionDetails} />
+                <TextField className={exampleChildClass} label="Filter by name:" onChange={this._onFilter} styles={textFieldStyles} />
+                <Announced message={`Number of items after filter applied: ${items.length}.`} />
+                <MarqueeSelection selection={this._selection}>
+                    <DetailsList
+                        items={items}
+                        columns={this._columns}
+                        setKey="set"
+                        layoutMode={DetailsListLayoutMode.justified}
+                        selection={this._selection}
+                        selectionPreservedOnEmptyClick={true}
+                        ariaLabelForSelectionColumn="Toggle selection"
+                        ariaLabelForSelectAllCheckbox="Toggle selection for all items"
+                        checkButtonAriaLabel="select row"
+                        onItemInvoked={this._onItemInvoked}
+                    />
+                </MarqueeSelection>
+            </div>
+        );
+    }
+
+    private _getSelectionDetails(): string {
+        const selectionCount = this._selection.getSelectedCount();
+
+        switch (selectionCount) {
+            case 0:
+                return "No items selected";
+            case 1:
+                return "1 item selected: " + (this._selection.getSelection()[0] as IUserListItem).name;
+            default:
+                return `${selectionCount} items selected`;
+        }
+    }
+
+    private _onFilter = (ev: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, text: string | undefined): void => {
+        this.setState({
+            items: text ? this._allItems.filter(i => i.name.toLowerCase().indexOf(text.toLowerCase()) > -1) : this._allItems
+        });
     };
-  }
 
-  public render(): JSX.Element {
-    const { items, selectionDetails } = this.state;
-
-    return (
-      <div>
-        <div className={exampleChildClass}>{selectionDetails}</div>
-        <Text>
-          Note: While focusing a row, pressing enter or double clicking will execute onItemInvoked, which in this
-          example will show an alert.
-        </Text>
-        <Announced message={selectionDetails} />
-        <TextField
-          className={exampleChildClass}
-          label="Filter by name:"
-          onChange={this._onFilter}
-          styles={textFieldStyles}
-        />
-        <Announced message={`Number of items after filter applied: ${items.length}.`} />
-        <MarqueeSelection selection={this._selection}>
-          <DetailsList
-            items={items}
-            columns={this._columns}
-            setKey="set"
-            layoutMode={DetailsListLayoutMode.justified}
-            selection={this._selection}
-            selectionPreservedOnEmptyClick={true}
-            ariaLabelForSelectionColumn="Toggle selection"
-            ariaLabelForSelectAllCheckbox="Toggle selection for all items"
-            checkButtonAriaLabel="select row"
-            onItemInvoked={this._onItemInvoked}
-          />
-        </MarqueeSelection>
-      </div>
-    );
-  }
-
-  private _getSelectionDetails(): string {
-    const selectionCount = this._selection.getSelectedCount();
-
-    switch (selectionCount) {
-      case 0:
-        return 'No items selected';
-      case 1:
-        return '1 item selected: ' + (this._selection.getSelection()[0] as IDetailsListBasicExampleItem).name;
-      default:
-        return `${selectionCount} items selected`;
-    }
-  }
-
-  private _onFilter = (ev: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, text: string | undefined): void => {
-    this.setState({
-      items: text ? this._allItems.filter(i => i.name.toLowerCase().indexOf(text) > -1) : this._allItems,
-    });
-  };
-
-  private _onItemInvoked = (item: IDetailsListBasicExampleItem): void => {
-    alert(`Item invoked: ${item.name}`);
-  };
+    private _onItemInvoked = (item: IUserListItem): void => {
+        alert(`Item invoked: ${item.name}`);
+    };
 }
+const iconStyle = {
+    icon: { color: "black" },
+    root: {
+        selectors: {
+            ":hover .ms-Button-icon": {
+                color: "rgb(0, 120, 212);"
+            }
+        }
+    }
+};
 
+const actions: React.FC = () => {
+    return (
+        <div>
+            <IconButton styles={iconStyle} iconProps={{ iconName: "Chart" }} title="Show thought process" ariaLabel="Show thought process" onClick={() => {}} />
+            <IconButton
+                styles={iconStyle}
+                iconProps={{ iconName: "Settings" }}
+                title="Show thought process"
+                ariaLabel="Show thought process"
+                onClick={() => {}}
+            />
+            <IconButton
+                styles={iconStyle}
+                iconProps={{ iconName: "Delete" }}
+                title="Show thought process"
+                ariaLabel="Show thought process"
+                onClick={() => {}}
+            />
+        </div>
+    );
+};
 
 const Admin: React.FC = () => {
     return (
         <div className={styles.page_container}>
-            <h1>Role and access</h1>
-            <Separator className={styles.separator} />
-            <div id="options-row">
-                <PrimaryButton className={styles.option} text="Create user" />
+            <div id="options-row" className={styles.row}>
+                <h1>Roles and access</h1>
+                <PrimaryButton className={styles.option} text="Invite user" />
             </div>
             <div>
-                <DetailsListBasicExample />
-                
+                <UserList />
             </div>
         </div>
     );
