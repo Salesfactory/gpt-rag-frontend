36727a007b96e3fa1d1a29efa635a46dca44af70
Manuel Castro <maacaro@gmail.com>
2024-11-19 17:48:50 -0400

    FA-209 implment RBCA and susbcription tiers Access (#155)

---
+++ frontend/src/pages/resources/UploadResources.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UploadResources: React.FC = () => {
+    return (
+        <div>
+            <h1>Upload Resources</h1>
+            <p>Welcome to the Upload Resources page!</p>
+        </div>
+    );
+};
+
+export default UploadResources;
