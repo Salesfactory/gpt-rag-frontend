9a6c3e71b6d489d86ec758d57cb9446706adfa2d
Manuel Castro <maacaro@gmail.com>
2025-01-22 18:16:29 -0400

    FA default timeout at config

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+    pageLoadTimeout: 120000, // Increase timeout to 2 minutes
+    defaultCommandTimeout: 10000,
+    e2e: {
+        baseUrl: "http://localhost:5173", // Set the base URL
+        setupNodeEvents(on, config) {
+            // Node event listeners
+        }
+    }
 });
