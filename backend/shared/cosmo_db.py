9dbf01bfb2b4c0152418602081fecfb9b465206e
ramirezmorac2 <ramirezmorac2@gmail.com>
2025-01-28 16:28:44 -0400

    * The function was created in cosmo_db to obtain all the companies that are in the "companyAnalysis" container.

--- backend/shared/cosmo_db.py
+++ backend/shared/cosmo_db.py
@@ -382,3 +382,40 @@ def patch_user_data(user_id, patch_data):
     except Exception as e:
         logging.error(f"Unexpected error while updating user data with id '{user_id}': {e}")
         raise e
+
+
+def get_company_list():
+    """
+    Retrieve all companies from the CosmosDB 'companyAnalysis' container.
+
+    Returns:
+        list: A list of company records from the database.
+
+    Raises:
+        NotFound: If no companies are found in the container.
+        Exception: For any unexpected errors during retrieval.
+    """
+
+    container = get_cosmos_container("companyAnalysis")
+
+    try:
+        items = list(container.query_items(
+            query="SELECT * FROM c",
+            enable_cross_partition_query=True
+        ))
+
+        if not items:
+            logging.warning(f"No companies found in the 'companyAnalysis' container.")
+            raise NotFound
+
+        logging.info(f"Companies successfully retrieved: {items}")
+        print(items)
+        return items
+
+    except CosmosResourceNotFoundError:
+        logging.warning(f"CosmosDB container not found or inaccessible.")
+        raise NotFound
+    
+    except Exception as e:
+        logging.error(f"Unexpected error retrieving Companies: {e}")
+        raise
