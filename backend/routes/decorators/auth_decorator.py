f6f90ce57db0a29e3d00df0c25202117215bbb12
Victor Maldonado <67714035+vmlcode@users.noreply.github.com>
2025-10-02 08:55:05 -0400

    hotfix auth decorator (#683)

--- backend/routes/decorators/auth_decorator.py
+++ backend/routes/decorators/auth_decorator.py
@@ -6,7 +6,12 @@ def auth_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         auth_instance = current_app.config.get("auth")
-        if not os.getenv("ENVIRONMENT") == "TEST":
-            return auth_instance.login_required(f)(*args, **kwargs)
+
+        if os.getenv("ENVIRONMENT") != "TEST" and auth_instance:
+            def wrapper_with_context(*a, context=None, **kw):
+                return f(*a, **kw)
+            return auth_instance.login_required(wrapper_with_context)(*args, **kwargs)
+
         return f(*args, **kwargs)
+
     return decorated_function
