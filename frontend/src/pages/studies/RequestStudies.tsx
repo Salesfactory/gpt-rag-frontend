9b0e2e8f8a223df130b0c55d79f8b12376e64834
vmlcode <67714035+vmlcode@users.noreply.github.com>
2025-11-06 18:17:13 -0400

    Remove session keep-alive monitoring logic from RequestStudies component

--- frontend/src/pages/studies/RequestStudies.tsx
+++ frontend/src/pages/studies/RequestStudies.tsx
@@ -3,24 +3,10 @@ import styles from "./RequestStudies.module.css"
 import { monitorSessionKeepAlive } from "../../api";
 
 const RequestStudies: React.FC = () => {
-
-    const [test, setTest] = useState(false)
-
-    useEffect(()=>{
-        if (test) {
-            const interval = setInterval(() => {
-                monitorSessionKeepAlive();
-            }, 10000); // 10 seconds
-
-            return () => clearInterval(interval);
-        }
-    }, [test])
-
     return (
         <div className={styles.page_container}>
             <h1>Request Studies</h1>
             <p>Welcome to the Request Studies page!</p>
-            <button onClick={() => {setTest(!test)}} >Monitor</button>
         </div>
     );
 };
