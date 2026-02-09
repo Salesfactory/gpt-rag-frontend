2235543a605e766d3da0f88de6d731a6ed12c995
ramirezmorac2 <ramirezmorac2@gmail.com>
2026-02-08 20:02:48 -0400

    FA-1283 Fix issue with the navbar

--- frontend/src/pages/layout/_Layoutcopy.tsx
+++ frontend/src/pages/layout/_Layoutcopy.tsx
@@ -19,7 +19,7 @@ const Layout: React.FC = () => {
             {/* Main Wrapper */}
             <div className={`${styles.bodyWrapper} ${styles.bodyWrapperFull} ${isCollapsed ? styles.collapsed : styles.expanded}`}>
                 {/* Header Start */}
-                <header className={`${styles.appHeader} border-bottom py-0.5 bg-white shadow-lg`}>
+                <header className={`${styles.appHeader} border-bottom py-0.5 bg-white`}>
                     <Navbar setIsCollapsed={setIsCollapsed} isCollapsed={isCollapsed} />
                 </header>
                 {/* Header End */}
