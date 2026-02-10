096f8f598ae852fbc7799d9c48849e89a5a99561
ramirezmorac2 <ramirezmorac2@gmail.com>
2026-02-10 09:26:05 -0400

    FA-1313 Update Team Management Table UI (#913)
    * FA-1313 Update team management table UI with improved styles and functionality
    
    * refactor: Rename "Create User" button to "Invite Team Member" in team management tests

--- frontend/cypress/e2e/team_management.cy.ts
+++ frontend/cypress/e2e/team_management.cy.ts
@@ -66,7 +66,7 @@ describe("Agent Section Tests", () => {
         cy.wait('@getUsers');
 
 
-        cy.get('button').contains("Create User").should("be.visible");
+        cy.get('button').contains("Invite Team Member").should("be.visible");
         cy.get('button').contains("All Roles").should("be.visible");
 
         //Table content check
@@ -78,8 +78,8 @@ describe("Agent Section Tests", () => {
         cy.get('span').should('contain.text', 'Adamska');
         cy.get('span').should('contain.text', 'No Account');
 
-        //Test for the Create User Modal
-        cy.get('button').contains("Create User").click();
+        //Test for the Invite Team Member Modal
+        cy.get('button').contains("Invite Team Member").click();
         cy.get('button').contains("Send Invitation").should("be.visible");
         cy.get('button').contains("Cancel").should("be.visible");
         cy.get('button').contains("Send Invitation").click();
